c30914a5d9c9dfa991df6b922dfda40bb4d2e15c
Bst1809 <bastien.selzner@gmail.com>
2022-09-27 15:45:55 +0200

    Refactor and changed search bookcard creation method

--- myBooks.js
+++ myBooks.js
@@ -1,55 +1,61 @@
-// Creation of buttons in DOM
+// Buttons  
 const addButton = document.createElement("button");
-const searchButton = document.createElement("button");
-const cancelButton = document.createElement("button");
-cancelButton.setAttribute("id", "cancelButton")
-
-// Buttons' name
 addButton.innerHTML = "Ajouter un livre";
+const searchButton = document.createElement("button");
 searchButton.innerHTML = "Rechercher";
+const cancelButton = document.createElement("button");
 cancelButton.innerHTML = "Annuler";
-
-// Append add button to <hr>
+cancelButton.id = "cancelButton";
+    // Append Buttons to <hr>
 const hr = document.getElementsByTagName("hr")[0];
 hr.appendChild(addButton);
 
-// Creation of labels for search inputs
+// Search Inputs
+    // Title input
 const titleLabel = document.createElement("h3");
 titleLabel.innerHTML = "Titre du livre : ";
-
-const authorLabel = document.createElement("h3");
-authorLabel.innerHTML = "Auteur-e du livre : ";
-
-// Creation of search inputs in DOM
-    //Title Input
 const titleInput = document.createElement("input");
 titleInput.setAttribute("id", "titleInput");
 titleInput.setAttribute("type", "search");
 titleInput.placeholder = "Exemple : 1984";
-    // Author Input
+    // Author input
+const authorLabel = document.createElement("h3");
+authorLabel.innerHTML = "Auteur-e : ";
 const authorInput = document.createElement("input");
 authorInput.setAttribute("id","authorInput");
 authorInput.setAttribute("type", "search");
 authorInput.placeholder = "Exemple : George Orwell";
 
-// Creation of div container for favorite list
+// Div for Favorite List (poch'liste) and search list 
+    // Search list
+const searchListBox = document.createElement("div");
+searchListBox.id = "searchListBox"
+const searchListTitle = document.createElement("h2");
+searchListTitle.innerHTML = "Ma Recherche";
+searchListBox.appendChild(searchListTitle);
+const searchList = document.createElement("div");
+searchList.setAttribute("id","searchList");
+searchListBox.appendChild(searchList);
+content.prepend(searchListBox);
+document.getElementById("searchListBox").style.display = "none"
+    // Favorite list
 const favoriteList = document.createElement("div");
 favoriteList.setAttribute("id","favoriteList");
 content.appendChild(favoriteList);
 
-// Creation of div container for favorite list
-const searchList = document.createElement("div");
-searchList.setAttribute("id","searchList");
-content.appendChild(searchList);
+const addToFavorite = () => {
+    favoriteBook.push(book);
+    sessionStorage.setItem("bookmarkedBook", JSON.stringify(favoriteBook));
+    };
 
-
-
-// Event handler : getting favorite books when loading page
+// Event handler : WHEN page loads => display favorite list 
 window.onload = () => {
 let favoriteBook = JSON.parse(sessionStorage.getItem("bookmarkedBook"));
     if (!favoriteBook){
-    }else{
-        for (let i = 0; i < favoriteBook.length; i++){
+    }else
+    {
+        for (let i = 0; i < favoriteBook.length; i++)
+        {
             const favoriteBookCard = document.createElement("section");
             favoriteBookCard.id = "favorite" + favoriteBook[i].id;
             favoriteBookCard.innerHTML =
@@ -58,39 +64,35 @@ let favoriteBook = JSON.parse(sessionStorage.getItem("bookmarkedBook"));
             </div>
             <div id = "txtBox"> 
             <h3>${favoriteBook[i].title}</h3>
-            <div id="id">Id : ${favoriteBook[i].id}</div>
-            <div id="author">Auteur-e : ${favoriteBook[i].author}</div>
-            <div id="description">${favoriteBook[i].description}</div>
+            <div id="id"><strong>Id : </strong>${favoriteBook[i].id}</div>
+            <div id="author"><strong>Auteur-e : </strong>${favoriteBook[i].author}</div>
+            <div id="description"><p>${favoriteBook[i].description}</p></div>
             </div>`;
 
             favoriteList.appendChild(favoriteBookCard);
 
             // Creation of Bin button to delete from Favorite 
             const binButton = document.createElement("i");
-            binButton.setAttribute("class", "fa-solid fa-trash-can fa-4x");
+            binButton.setAttribute("class", "fa-solid fa-trash-can fa-3x");
             binButton.setAttribute("id", "binButton");
             favoriteBookCard.appendChild(binButton);
 
-            
-
-             // Event handler : CLICK on binButton to delete from favorite list (within search button handler)
-            binButton.addEventListener('click', function () {
-            let favoriteBook = JSON.parse(sessionStorage.getItem("bookmarkedBook"));
-            const bookToDelete = document.getElementById("favorite"+favoriteBook[i].id);
-
-            favoriteList.removeChild(bookToDelete);
-            favoriteBook = favoriteBook.filter((b) => b.id != favoriteBook[i].id);
-            sessionStorage.setItem("bookmarkedBook", JSON.stringify(favoriteBook));
-            location.reload(true);
+             // Event handler : WHEN click on binButton => delete book from favorite list
+            binButton.addEventListener('click', function () 
+            {   // Remove from display
+                let favoriteBook = JSON.parse(sessionStorage.getItem("bookmarkedBook"));
+                const bookToDelete = document.getElementById("favorite"+favoriteBook[i].id);
+                favoriteList.removeChild(bookToDelete);
+                // Remove from array
+                favoriteBook = favoriteBook.filter((book) => book.id != favoriteBook[i].id);
+                sessionStorage.setItem("bookmarkedBook", JSON.stringify(favoriteBook));
+                location.reload(true);
             });
-    
-            
         }
     }
+}
 
- }
-
-// Event handler : CLICK on addButton => addButton is replaced by searchButton and cancelButton
+// Event handler : WHEN click on addButton => addButton is replaced by searchButton and cancelButton
 addButton.addEventListener ("click", function(){
     hr.removeChild(addButton);
     hr.appendChild(titleLabel);
@@ -101,7 +103,7 @@ addButton.addEventListener ("click", function(){
     hr.appendChild(cancelButton);
 });
 
-// Event handler : CLICK on cancelButton => Clear <hr> and regeneration of addButton
+// Event handler : WHEN click on cancelButton => Clear <hr> and Search list + regeneration of addButton + switch h2 title + reload page
 cancelButton.addEventListener ("click", function(){
     document.querySelector("hr").innerHTML = "";
     document.getElementById("searchList").innerHTML = "";
@@ -111,7 +113,7 @@ cancelButton.addEventListener ("click", function(){
 
 });
 
-// Event handler : CLICK on searchButton
+// Event handler : WHEN click on searchButton => search and display book whith Google Books API
 searchButton.addEventListener("click", async function() {
 
     let searchTitle = document.getElementById("titleInput").value; 
@@ -119,7 +121,8 @@ searchButton.addEventListener("click", async function() {
 
     if (!searchTitle || !searchAuthor) {
         alert("Merci de remplir les champs titre ET auteur-e");
-    } else {   
+    }else 
+    {   
 
         const response = await fetch("https://www.googleapis.com/books/v1/volumes?q="+searchTitle + "+inauthor:" + searchAuthor + "&key=AIzaSyC4BzkgE2fpVlMpcAJBSx_YEYclTDQLjTU");
         const bookData = await response.json();
@@ -127,11 +130,11 @@ searchButton.addEventListener("click", async function() {
         if (bookData.totalItems === 0) {
             alert("Aucun livre n'a été trouvé");
 
-        } else {
-            if (document.getElementById("favoriteList")){
-                content.replaceChild(searchList,favoriteList);
-            }else{} 
+        } else 
+        {
+            
             // Replace title "Ma Poch'list" with "Ma Recherche" and clear previous search
+            document.getElementById("searchListBox").style.display = "initial"
             document.getElementsByTagName("h2")[1].innerHTML = "Ma Recherche";
             document.getElementById("searchList").innerHTML = "";
 
@@ -145,33 +148,51 @@ searchButton.addEventListener("click", async function() {
                     description : bookData?.searchInfo?.textSnippet?bookData.searchInfo.textSnippet:"Information manquante",
                     img : bookData?.volumeInfo?.imageLinks?.thumbnail?bookData.volumeInfo.imageLinks.thumbnail:"Images/unavailable_thumbnail.jpg",
                   }
-
-                // Creation of book card
+                // Create book card
                 const bookBox = document.createElement("section");
-                bookBox.innerHTML =
-                `<div id = "imgBox">
-                <div id="image"> <img src="${book.img}"/> </div>
-                </div>
-                <div id = "txtBox"> 
-                <h3>${book.title}</h3>
-                <div id="id">Id : ${book.id}</div>
-                <div id="author">Auteur-e : ${book.author}</div>
-                <div id="description">${book.description}</div>
-                </div>`;
-
-                // Creation of Bookmark button to add to Favorite 
+                const imgBox = document.createElement("div");
+                imgBox.id = "imgBox";
+                const cardImg = document.createElement("img");
+                cardImg.id = "cardImg";
+                cardImg.src = book.img;
+                bookBox.appendChild(imgBox);
+                imgBox.appendChild(cardImg);
+                const txtBox = document.createElement("div");
+                txtBox.id = "txtBox";
                 const bookmark = document.createElement("i");
-                bookmark.setAttribute("class", "fa-regular fa-bookmark fa-4x");
-                bookmark.setAttribute("id", "bookmarkAdd");
-                bookBox.appendChild(bookmark);
+                bookmark.setAttribute("class", "fa-regular fa-heart fa-3x");
+                bookmark.id = "bookmarkAdd";
+                txtBox.appendChild(bookmark);
+                const cardHeader = document.createElement("h3");
+                cardHeader.innerText = book.title;
+                txtBox.appendChild(cardHeader);
+                const cardId = document.createElement("div");
+                cardId.innerText = book.id;
+                txtBox.appendChild(cardId);
+                const cardAuthor = document.createElement("p");
+                cardAuthor.innerText = "Auteur : " + book.author;
+                    if (book.author > 1) {
+                        book.author = book.author.slice(0, 2);
+                    }
+                txtBox.appendChild(cardAuthor);
+                const cardDescription = document.createElement("p");
+                cardDescription.innerText = book.description;
+                    if (cardDescription.innerText.length > 200) {
+                        cardDescription.innerText = cardDescription.innerText.substring(0, 200) + '...';
+                    }
+                txtBox.appendChild(cardDescription);
+                bookBox.appendChild(txtBox);
+                
 
-                // Event handler : CLICK on bookmark to add to favorite list (within search button handler)
+                
+
+                // Event handler : WHEN click on bookmark => add to favorite list
                 bookmark.addEventListener ("click", () => {
                     // Function : add book to favorite list 
                     const addToFavorite = () => {
                     favoriteBook.push(book);
                     sessionStorage.setItem("bookmarkedBook", JSON.stringify(favoriteBook));
-                };
+                    };
                     let favoriteBook = JSON.parse(sessionStorage.getItem("bookmarkedBook"));
                     if (favoriteBook == null){
                         favoriteBook = [];
@@ -184,14 +205,10 @@ searchButton.addEventListener("click", async function() {
                         addToFavorite();
                         }
                     }                                                    
-                })
-                                    
+                })                       
                 searchList.appendChild(bookBox);
-
             });
-
-        } 
-                
+        }       
     }
 })
 
